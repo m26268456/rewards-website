906102f83cd0d3330edebad332e9b03652b8f09d
m26268456 <m26268456@gmail.com>
2025-12-06 16:36:31 +0800

    Add files via upload

--- frontend/src/pages/settings/QuotaManagement.tsx
+++ frontend/src/pages/settings/QuotaManagement.tsx
@@ -287,6 +287,7 @@ export default function QuotaManagement() {
                   </tr>
                 );
               });
+              });
             })}
           </tbody>
         </table>
