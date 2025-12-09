32de4691fad24ff26edabdcdb816c8148c6f58af
m26268456 <m26268456@gmail.com>
2025-12-09 23:07:05 +0800

    Add files via upload

--- backend/src/services/schemeService.ts
+++ backend/src/services/schemeService.ts
@@ -472,6 +472,7 @@ export async function queryChannelRewards(
           schemeChannelName,
           rewardItems,
           totalCalculatedReward: null,
+          sourceChannelName: channelName,
         };
       });
 
